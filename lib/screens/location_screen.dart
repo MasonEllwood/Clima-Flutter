10dd265c677f766a91da1f742c1a2a034a8632f4
Mason Ellwood <masonellwood@gmail.com>
2020-03-01 15:06:41 -0700

    typed value pulls weather

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -76,11 +76,21 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return CityScreen();
-                      }));
+                    onPressed: () async {
+                      var typedName = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return CityScreen();
+                          },
+                        ),
+                      );
+                      if (typedName != null) {
+                        var weatherData =
+                            await weather.getCityWeather(typedName);
+                        updateUI(weatherData);
+                      }
+                      print(typedName);
                     },
                     child: Icon(
                       Icons.location_city,
